219f6c51f192644a708039257ae7be23197af9c6
Marcello <mdefeo@users.noreply.github.com>
2023-03-31 19:20:23 -0400

    Feedback 1

--- assets/built/main.bundle.js
+++ assets/built/main.bundle.js
@@ -7,26 +7,8 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (function() { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./assets/js/webpack.entry.js":
-/*!************************************!*\
-  !*** ./assets/js/webpack.entry.js ***!
-  \************************************/
-/***/ (function() {
-
-eval("\n\n//# sourceURL=webpack://bsh/./assets/js/webpack.entry.js?");
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
+/******/ 	"use strict";
 /******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./assets/js/webpack.entry.js"]();
 /******/ 	
 /******/ })()
 ;
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
 const path = require('path');
 var webpack = require('webpack');  
 var autoprefixer = require('autoprefixer');
+
 module.exports = {
     mode: 'development',
+    plugins: [
+        new webpack.DefinePlugin({
+          'process.env.HYVOR_SSO_KEY': JSON.stringify("988bf47f56422d08b140ac8ae1142aff"),
+          'process.env.HYVOR_WEB_ID': JSON.stringify("8652"),
+          'process.env.BSH_DOMAIN': JSON.stringify("https://broadstreethockey.com/"),
+          'process.env.BSH_DOMAIN_STAGING': JSON.stringify("https://broad-street-hockey.ghost.io/"),
+          'process.env.BSH_DOMAIN_DEV': JSON.stringify("http://localhost:2368"),
+        })
+    ],
     module: {
         rules: [
             {
